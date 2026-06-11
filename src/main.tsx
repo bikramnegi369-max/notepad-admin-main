d1060c74e88d93c89d37b0a297a795d5b9a674b9
Bikram Singh Negi <bikramnegi369@gmail.com>
2026-06-11 14:55:49 +0530

    toast fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,25 @@ import './css/satoshi.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import { store } from './redux/store';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, Flip } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS for react-toastify
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <Router>
-      <ToastContainer />
+      <ToastContainer
+        position="top-right" // Position the toast notifications to the top-right of the screen
+        autoClose={5000} // Automatically close toasts after 5 seconds
+        hideProgressBar={false} // Show a progress bar indicating when the toast will close
+        newestOnTop={true} // Newest toast appears on top of older ones
+        closeOnClick // Allow toasts to be dismissed by clicking them
+        rtl={false} // Disable right-to-left support
+        pauseOnFocusLoss // Pause autoClose timer when the window loses focus
+        draggable // Allow toasts to be dragged
+        pauseOnHover // Pause autoClose timer when hovering over a toast
+        theme="light" // Use the light theme for toasts
+        transition={Flip} // Add a flip transition effect for toasts
+      />
       <App />
     </Router>
   </Provider>,
